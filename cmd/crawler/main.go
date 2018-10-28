81f19aa396b43a1be02adc8388a04a35068b8e96
Mitya Teryaew <mitya.teryaew@gmail.com>
2018-10-28 15:57:23 +0300

    feat(*): update crawler

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -14,24 +14,44 @@ var (
 
 func main() {
   flag.Parse()
+
+  var (
+    sem = make(chan struct{}, *parallelism)
+    work = make(chan *url.URL)
+  )
+
   for _, s := range flag.Args() {
     u, err := url.ParseRequestURI(s)
     if err != nil {
       log.Fatalf("invalid url %q: %v", s, err)
     }
 
+    select {
+    case work <- u:
+      default:
+        select {
+        case sem <- struct{}{}:
+          go process(work, u)
+        case work <- u:
+        }
+    }
+  }
+}
+
+func process(work <-chan *url.URL, u *url.URL) {
+  for {
     resp, err := http.Get(u.String())
     if err != nil {
       log.Printf("error doing request for %q: %v", u, err)
       continue
     }
-
     dumpResponse(resp)
+    u = <-work
   }
 }
 
 func dumpResponse(resp *http.Response) {
-  dump, err := httputil.DumpResponse(resp, true)
+  dump, err := httputil.DumpResponse(resp, false)
   if err != nil {
     log.Printf("can not dump request from %q", resp.Request.URL)
     return
